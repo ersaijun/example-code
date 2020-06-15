a37480f6dff9a011a5087dd714cadf6f3a0484fc
zhangsaii@163.com <zhangsaii@163.com>
2020-06-15 23:22:07 +0800

    01 prac

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -28,7 +28,6 @@ def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
 
-
 if __name__ == '__main__':
     deck = FrenchDeck()
 
